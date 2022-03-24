57f6d3985797a0a6650947b570a05f9a6b609850
unknown <viniciusdeab@outlook.com>
2022-03-24 16:47:29 -0300

    Ajuste

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const inputElement = document.querySelector(".new-task-input");
-const addTaskButton = document.querySelector("new-task-button");
+const addTaskButton = document.querySelector(".new-task-button");
 
 const validateInput = () => inputElement.value.trim().length > 0;
 
 const handleAddTask = () => {
     const inputIsValid = validateInput();
     
-    if(inputIsValid){
+    if(!inputIsValid){
         return inputElement.classList.add("error");
     }
-}
+};
 
 addTaskButton.addEventListener("click", () => handleAddTask());
