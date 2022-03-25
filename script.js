f24f9e5c3b7c84a8972fcf2155e91777724ec1cb
unknown <viniciusdeab@outlook.com>
2022-03-25 12:26:54 -0300

    configuração da conclusão da tarefa

--- script.js
+++ script.js
@@ -18,15 +18,32 @@ const handleAddTask = () => {
     const taskContent = document.createElement("p");
     taskContent.innerText = inputElement.value;
 
+    taskContent.addEventListener("click", () => handleClick(taskContent));
+
+
     const deleteItem = document.createElement("i");
     deleteItem.classList.add("fa-solid");
     deleteItem.classList.add("fa-trash-can");
+
+    deleteItem.addEventListener("click", () => handleDeleteClick());
 
     taskItemContainer.appendChild(taskContent);
     taskItemContainer.appendChild(deleteItem);
 
     tasksContainer.appendChild(taskItemContainer);
+
+    inputElement.value = " ";
 };
+
+const handleClick = (taskContent) => {
+    const tasks = tasksContainer.childNodes;
+
+    for(const task of tasks){
+        if(task.firstChild.isSameNode(taskContent)){
+            task.firstChild.classList.toggle("completed");
+        }
+    }
+}
 
 const handleInputChange = () => {
     const inputIsValid = validateInput();
