78a74e9421b3c0140797c50d92a3322b56ef999e
unknown <viniciusdeab@outlook.com>
2022-03-25 11:32:22 -0300

    Adição da tarefas, com a adição das divs para criação das tarefas e icone de delete

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const inputElement = document.querySelector(".new-task-input");
 const addTaskButton = document.querySelector(".new-task-button");
 
+const tasksContainer = document.querySelector(".tasks-container");
+
 const validateInput = () => inputElement.value.trim().length > 0;
 
 const handleAddTask = () => {
@@ -9,6 +11,21 @@ const handleAddTask = () => {
     if(!inputIsValid){
         return inputElement.classList.add("error");
     }
+
+    const taskItemContainer = document.createElement("div");
+    taskItemContainer.classList.add("task-item");
+
+    const taskContent = document.createElement("p");
+    taskContent.innerText = inputElement.value;
+
+    const deleteItem = document.createElement("i");
+    deleteItem.classList.add("fa-solid");
+    deleteItem.classList.add("fa-trash-can");
+
+    taskItemContainer.appendChild(taskContent);
+    taskItemContainer.appendChild(deleteItem);
+
+    tasksContainer.appendChild(taskItemContainer);
 };
 
 const handleInputChange = () => {
@@ -17,7 +34,7 @@ const handleInputChange = () => {
     if(inputIsValid) {
         return inputElement.classList.remove("error");
     }
-}
+};
 
 addTaskButton.addEventListener("click", () => handleAddTask());
 
